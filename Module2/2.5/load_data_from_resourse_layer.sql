058050e418e9282cb57a34fd13c54146e0799810
Gleb Budnik <budnikgleb@mail.ru>
2023-09-25 17:04:40 +1000

    change module 2

--- Module2/2.5/load_data_from_resourse_layer.sql
+++ Module2/2.5/load_data_from_resourse_layer.sql
@@ -7,8 +7,7 @@ INSERT INTO company.ship_mode(ship_name)
 SELECT DISTINCT ship_mode
 FROM orders
 WHERE ship_mode not in (SELECT ship_mode
-					   FROM company.ship_mode);
-
+                       FROM company.ship_mode);
 
 INSERT INTO company.product(product_name, category, subcategory)
 SELECT DISTINCT product_name, category, subcategory
@@ -24,8 +23,8 @@ AND o.category = cp.category
 AND o.subcategory = cp.subcategory
 WHERE cp.product_name IS NULL AND cp.category IS NULL AND cp.subcategory IS NULL;
 
-					   
-INSERT INTO company.customer(customer_id_original, customer_name, segment)					   
+                       
+INSERT INTO company.customer(customer_id_original, customer_name, segment)                     
 SELECT DISTINCT customer_id, customer_name, segment
 FROM orders;
 
@@ -39,8 +38,8 @@ AND o.customer_name = cc.customer_name
 AND o.segment = cc.segment
 WHERE cc.customer_id_original IS NULL AND cc.customer_name IS NULL;
 
-				   
-INSERT INTO company.address_data(country, state, city, postal_code)					   
+                   
+INSERT INTO company.address_data(country, state, city, postal_code)                    
 SELECT DISTINCT country, state, city, postal_code
 FROM orders;
 
@@ -58,8 +57,8 @@ AND ca.state IS NULL
 AND ca.city IS NULL
 AND ca.postal_code IS NULL;
 
-					   
-INSERT INTO company.region(region, person)					   
+                       
+INSERT INTO company.region(region, person)                     
 SELECT DISTINCT region, person
 FROM people;
 
@@ -72,7 +71,6 @@ AND o.person = cr.person
 WHERE o.region IS NULL
 AND o.person IS NULL;
 
-
 INSERT INTO company.returned(returned)
 SELECT DISTINCT returned
 FROM returns;
@@ -81,22 +79,22 @@ INSERT INTO company.returned(returned)
 SELECT DISTINCT r.returned
 FROM returns  AS r
 WHERE r.returned not in (SELECT DISTINCT returned
-						 FROM company.returned);
+                         FROM company.returned);
 
 SELECT DISTINCT o.order_id, 
-	   r.returned,
-	   o.sales,
-	   o.profit,
-	   o.discount,
-	   o.order_date,
-	   o.ship_date,
-	   cr.pk_region_region_id AS fk_region_id, 
-	   ca.pk_address_data_id AS fk_address_data_id,
-	   cc.customer_id AS fk_customer_id,
-	   cs.pk_ship_mode_id AS fk_ship_mode_id,
-	   cp.pk_product_id AS fk_product_id, 
-	   crt.pk_returned_id AS fk_returned_id
-	   
+       r.returned,
+       o.sales,
+       o.profit,
+       o.discount,
+       o.order_date,
+       o.ship_date,
+       cr.region_region_id AS region_id, 
+       ca.address_data_id AS address_data_id,
+       cc.customer_id AS customer_id,
+       cs.ship_mode_id AS ship_mode_id,
+       cp.product_id AS product_id, 
+       crt.returned_id AS returned_id
+       
 FROM orders AS o
 LEFT JOIN returns r ON o.order_id = r.order_id
 LEFT JOIN people p ON o.region = p.region
@@ -106,33 +104,32 @@ LEFT JOIN company.customer cc ON o.customer_id = cc.customer_id_original
 LEFT JOIN company.ship_mode cs ON o.ship_mode = cs.ship_name
 LEFT JOIN company.product AS cp ON o.product_name = cp.product_name AND o.category = cp.category AND o.subcategory = cp.subcategory
 LEFT JOIN company.returned AS crt ON r.returned = crt.returned;
-
 
 INSERT INTO company.sales(order_id, 
-						  sales, 
-						  profit, 
-						  discount, 
-						  order_date, 
-						  ship_date, 
-						  fk_region_id, 
-						  fk_address_data_id,
-						  fk_customer_id,
-						  fk_ship_mode_id,
-						  fk_product_id,
-						  fk_returned_id)
+                          sales, 
+                          profit, 
+                          discount, 
+                          order_date, 
+                          ship_date, 
+                          region_id, 
+                          address_data_id,
+                          customer_id,
+                          ship_mode_id,
+                          product_id,
+                          returned_id)
 SELECT DISTINCT o.order_id, 
-	   o.sales,
-	   o.profit,
-	   o.discount,
-	   o.order_date,
-	   o.ship_date,
-	   cr.pk_region_region_id AS fk_region_id, 
-	   ca.pk_address_data_id AS fk_address_data_id,
-	   cc.customer_id AS fk_customer_id,
-	   cs.pk_ship_mode_id AS fk_ship_mode_id,
-	   cp.pk_product_id AS fk_product_id, 
-	   crt.pk_returned_id AS fk_returned_id
-	   
+       o.sales,
+       o.profit,
+       o.discount,
+       o.order_date,
+       o.ship_date,
+       cr.region_region_id AS region_id, 
+       ca.address_data_id AS address_data_id,
+       cc.customer_id AS customer_id,
+       cs.ship_mode_id AS ship_mode_id,
+       cp.product_id AS product_id, 
+       crt.returned_id AS returned_id
+       
 FROM orders AS o
 LEFT JOIN returns r ON o.order_id = r.order_id
 LEFT JOIN people p ON o.region = p.region
@@ -142,8 +139,6 @@ LEFT JOIN company.customer cc ON o.customer_id = cc.customer_id_original
 LEFT JOIN company.ship_mode cs ON o.ship_mode = cs.ship_name
 LEFT JOIN company.product AS cp ON o.product_name = cp.product_name AND o.category = cp.category AND o.subcategory = cp.subcategory
 LEFT JOIN company.returned AS crt ON r.returned = crt.returned;
-
 
 SELECT *
 FROM company.sales
-
--- Module2/2.5/request.sql
+++ Module2/2.5/request.sql
@@ -10,7 +10,7 @@ FROM company.sales;
 SELECT SUM(profit) / count(*) 
 FROM company.sales;
 
-SELECT SUM(sales) / COUNT(DISTINCT fk_customer_id)
+SELECT SUM(sales) / COUNT(DISTINCT customer_id)
 FROM company.sales;
 
 -- Avg. Discount
@@ -20,46 +20,43 @@ FROM company.sales;
 -- Monthly Sales by Segment
 SELECT DATE_TRUNC('month', order_date)::date, segment, sum(sales)
 FROM company.sales as s
-left join company.customer as cc on s.fk_customer_id = cc.customer_id
+left join company.customer as cc on s.customer_id = cc.customer_id
 GROUP BY DATE_TRUNC('month', order_date)::date, segment
 ORDER BY DATE_TRUNC('month', order_date)::date, segment;
 
 -- Monthly Sales by Product Category
 SELECT DATE_TRUNC('month', order_date)::date, category, sum(sales)
 FROM company.sales as s
-left join company.product as cp on s.fk_product_id = cp.pk_product_id
+left join company.product as cp on s.product_id = cp.product_id
 GROUP BY DATE_TRUNC('month', order_date)::date, category
 ORDER BY DATE_TRUNC('month', order_date)::date, category;
-
 
 -- Sales by Product Category over time (Продажи по категориям)
 SELECT category, sum(sales), count(sales)
 FROM company.sales as s
-left join company.product as cp on s.fk_product_id = cp.pk_product_id
+left join company.product as cp on s.product_id = cp.product_id
 GROUP BY category
-ORDER BY sum(sales), count(sales)
+ORDER BY sum(sales), count(sales);
 
 -- Customer Analysis
 
 -- Sales and Profit by Customer
-SELECT customer_id, customer_name, SUM(Sales) as sum_sales, SUM(profit) as sum_profit
+SELECT s.customer_id, customer_name, SUM(Sales) as sum_sales, SUM(profit) as sum_profit
 FROM company.sales as s
-left join company.customer as cc on s.fk_customer_id = cc.customer_id
-GROUP BY customer_id, customer_name
-
+left join company.customer as cc on s.customer_id = cc.customer_id
+GROUP BY s.customer_id, customer_name;
 
 -- Customer Ranking
 SELECT *, RANK() OVER(ORDER BY sum_sales DESC)
 FROM (
-	SELECT customer_id, customer_name, SUM(Sales) as sum_sales
-	FROM company.sales as s
-	left join company.customer as cc on s.fk_customer_id = cc.customer_id
-	GROUP BY customer_id, customer_name
-)  as table_1
+    SELECT s.customer_id, customer_name, SUM(Sales) as sum_sales
+    FROM company.sales as s
+    left join company.customer as cc on s.customer_id = cc.customer_id
+    GROUP BY s.customer_id, customer_name
+)  as table_1;
 
 -- Sales per region
 SELECT state, SUM(sales), COUNT(sales)
 FROM company.sales as s
-left join company.address_data as cad on s.fk_address_data_id = cad.pk_address_data_id
-GROUP BY state
-GROUP BY state
+left join company.address_data as cad on s.address_data_id = cad.address_data_id
+GROUP BY state;
